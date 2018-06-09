e3215bbf9e816426c0ba9d1beb31386d91e1529b
akshay0789 <habbiakshay1@gmail.com>
2018-06-09 16:22:49 +0000

    assignment10

---
+++ ass_10.py
@@ -0,0 +1,63 @@
+#1
+class animal:
+    def animal_attributes(self,legs):
+        print("animals have",legs,"legs")
+class tiger:
+    def tiger_attributes(sself,nutrition):
+        print("tiger is",nutrition)
+        tig=Tiger()
+        tig.animal_attributes(4)
+        tig.tiger_attributes('carnivores')
+
+
+#2
+AB
+AB
+
+#3
+class cop
+    def init (self,name,experiance):
+        self.name=name
+        self.experiance=experiance
+    def display_details(self):
+        print(self.name,"cope is on duty with",self.experiance,"year of experiance")
+        def update_details(self):
+            self.name='sherlock'
+            self.experiance=7
+class Mission(cop):
+    def init_(self):
+        super(mission,self) init()
+    def display_mission(self.mission):
+        print("the active mission is",mission)
+    def assign_cop(self,mission):
+        print(self.name,"has been assigned",mission)
+        mission = Mission('sherlock',4)
+        mission.display_mission(secret)
+        mission.display_mission('secret')
+        mission.display_details()
+        mission.assign_cop('secret')
+
+
+#4
+class shape:
+    def init (self,length,breadth):
+        self.length=length
+        self.breadth=breadth
+        def area(self):
+            print("the area is",self.length*self.breadth,"units")
+class rectangle(shape):
+    def init_(self):
+        super(ractangle,self)_init_()
+class square(shape):
+    def init_(self):
+        super(square,self) init ()
+        rect=shape(2,4)
+        rect.area()
+        sq=shape(4,4)
+        sq.area()
+
+
+
+
+
+
